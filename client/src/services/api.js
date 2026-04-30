8efc7bf7612cbbce034ffc46f751b9c876e83ecc
jeevitha <jeevithasbtech24@rvu.edu.in>
2026-04-30 16:32:58 +0530

    update API URL to production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://hiretracker-api.onrender.com/api";
 
 const getToken = () => {
   const user = JSON.parse(localStorage.getItem("ht_user") || "{}");
